e4aa2ed60829db592375b1aac0b37ba2e22a4cb6
CarlosCatedras <catedrasinsituto43@gmail.com>
2024-10-09 09:53:48 -0300

    Formularios de Ventas

---
+++ Bombones.Windows/Formularios/frmVentas.cs
@@ -0,0 +1,19 @@
+﻿namespace Bombones.Windows.Formularios
+{
+    public partial class frmVentas : Form
+    {
+        public frmVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVentas_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void tsbCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Bombones.Windows/Formularios/frmVentasAE.cs
@@ -0,0 +1,54 @@
+﻿namespace Bombones.Windows.Formularios
+{
+    public partial class frmVentasAE : Form
+    {
+
+        public frmVentasAE()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVentasAE_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void ExitItem_Click(object? sender, EventArgs e)
+        {
+            return;
+        }
+
+        private void EditItem_Click(object? sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteItem_Click(object? sender, EventArgs e)
+        {
+        }
+
+
+        private void BtnAgregar_Click(object? sender, EventArgs e)
+        {
+        }
+
+
+
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            CancelarCompra();
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CancelarCompra()
+        {
+        }
+
+
+    }
+}
